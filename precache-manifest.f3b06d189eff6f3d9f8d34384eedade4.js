e10e3b0a03bb6387750a357384a02c5d4a857b3b
unknown <khanhnqps10920@fpt.edu.vn>
2019-07-06 17:27:18 +0700

    init

---
+++ precache-manifest.f3b06d189eff6f3d9f8d34384eedade4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2ae66a60470189fa7c7e51077c9e18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9898a54be7642ffdd9b9",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9898a54be7642ffdd9b9",
+    "url": "./static/js/main.274e0f41.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
